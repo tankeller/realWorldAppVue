e9b60afc3af40a5a9e4e9dab93667b8615cc63f7
Tan Keller <ersoichso@gmail.com>
2019-07-15 20:48:00 +0200

    Add date filter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import date from './filters/date'
+
+Vue.filter('formatDate', date)
+
 Vue.config.productionTip = false
 
 new Vue({
