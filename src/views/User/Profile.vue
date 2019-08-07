7e042fd3a3114b520a8f9e29d672233a790a16d2
Tan Keller <ersoichso@gmail.com>
2019-08-07 14:22:49 +0200

    Add dynamic content to profile page

--- src/views/User/Profile.vue
+++ src/views/User/Profile.vue
@@ -4,13 +4,13 @@
       <div class="container">
         <div class="row">
           <div class="col-xs-12 col-md-10 offset-md-1">
-            <img src="http://i.imgur.com/Qr71crq.jpg" class="user-img">
-            <h4>Eric Simons</h4>
-            <p>Cofounder @GoThinkster, lived in Aol's HQ for a few months, kinda looks like Peeta from the Hunger Games</p>
+            <img v-if="user.image" :src="user.image" class="user-img" />
+            <h4>{{ user.username }}</h4>
+            <p>{{ user.bio }}</p>
             <button class="btn btn-sm btn-outline-secondary action-btn">
               <i class="ion-plus-round"></i>
               &nbsp;
-              Follow Eric Simons
+              Follow {{ user.username }}
             </button>
           </div>
         </div>
@@ -23,59 +23,72 @@
           <div class="articles-toggle">
             <ul class="nav nav-pills outline-active">
               <li class="nav-item">
-                <a class="nav-link active" href>My Articles</a>
+                <a 
+                class="nav-link active" 
+                href="#" 
+                @click.prevent="showOwnArticles = !showOwnArticles">
+                  My Articles
+                </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href>Favorited Articles</a>
+                <a
+                  class="nav-link"
+                  href="#"
+                  @click.prevent="showOwnArticles = !showOwnArticles">
+                    Favorited Articles
+                </a>
               </li>
             </ul>
           </div>
 
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href>
-                <img src="http://i.imgur.com/Qr71crq.jpg">
-              </a>
-              <div class="info">
-                <a href class="author">Eric Simons</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 29
-              </button>
-            </div>
-            <a href class="preview-link">
-              <h1>How to build webapps that scale</h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-            </a>
-          </div>
-
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href>
-                <img src="http://i.imgur.com/N4VcUeJ.jpg">
-              </a>
-              <div class="info">
-                <a href class="author">Albert Pai</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 32
-              </button>
-            </div>
-            <a href class="preview-link">
-              <h1>The song you won't ever stop singing. No matter how hard you try.</h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-              <ul class="tag-list">
-                <li class="tag-default tag-pill tag-outline">Music</li>
-                <li class="tag-default tag-pill tag-outline">Song</li>
-              </ul>
-            </a>
-          </div>
+          <template v-if="!loading">
+            <app-article-list 
+              v-if="showOwnArticles" 
+              :author="user.username">
+            </app-article-list>
+            <app-article-list
+              v-else
+              :author="user.username"
+              :showFavoritedArticles="true"
+            >
+            </app-article-list>
+          </template>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+import AppArticleList from "@/components/Listing/ArticleList.vue";
+
+export default {
+  data() {
+    return {
+      user: {},
+      loading: true,
+      showOwnArticles: true,
+    };
+  },
+  components: {
+    AppArticleList
+  },
+  created() {
+    axios
+      .get(
+        "https://conduit.productionready.io/api/profiles/" +
+          this.$route.params.username
+      )
+      .then(res => {
+        this.user = res.data.profile;
+        this.loading = false;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  computed: {}
+};
+</script>
