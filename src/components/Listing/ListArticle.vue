c45e67d05f7b22a09b8a9dba46637f2d69e997db
Tan Keller <ersoichso@gmail.com>
2019-08-07 13:52:47 +0200

    Only add img tag if author has image

--- src/components/Listing/ListArticle.vue
+++ src/components/Listing/ListArticle.vue
@@ -2,14 +2,15 @@
   <div class="article-preview">
     <div class="article-meta">
       <a href="profile.html">
-        <img :src="article.author.image">
+        <img v-if="article.author.image" :src="article.author.image" />
       </a>
       <div class="info">
         <a href class="author">{{ article.author.username }}</a>
         <span class="date">{{ article.createdAt | formatDate}}</span>
       </div>
       <button class="btn btn-outline-primary btn-sm pull-xs-right">
-        <i class="ion-heart"></i> {{ article.favoritesCount }}
+        <i class="ion-heart"></i>
+        {{ article.favoritesCount }}
       </button>
     </div>
     <a href class="preview-link">
@@ -17,16 +18,18 @@
       <p>{{ article.description }}</p>
       <span>Read more...</span>
       <ul class="tag-list">
-        <li :key="index" v-for="(tag, index) in article.tagList" class="tag-default tag-pill tag-outline ng-binding ng-scope">
-            {{ tag }}
-        </li>
-    </ul>
+        <li
+          :key="index"
+          v-for="(tag, index) in article.tagList"
+          class="tag-default tag-pill tag-outline ng-binding ng-scope"
+        >{{ tag }}</li>
+      </ul>
     </a>
   </div>
 </template>
 
 <script>
-    export default {
-        props: ['article']
-    }
+export default {
+  props: ["article"]
+};
 </script>
