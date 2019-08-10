7481c0e524d4ec4d832426532b114d01b8488857
Tan Keller <ersoichso@gmail.com>
2019-08-10 17:15:09 +0200

    Added axios request to register a user

--- src/views/User/Register.vue
+++ src/views/User/Register.vue
@@ -5,27 +5,62 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign up</h1>
           <p class="text-xs-center">
-            <a href>Have an account?</a>
+            <router-link :to="{name: 'login'}">Have an account?</router-link>
           </p>
 
-          <ul class="error-messages">
+          <ul v-if="false" class="error-messages">
             <li>That email is already taken</li>
           </ul>
 
-          <form>
+          <form @:submit.prevent="onSubmit">
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+              <input class="form-control form-control-lg" v-model="formData.name" type="text" placeholder="Your Name">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Email">
+              <input class="form-control form-control-lg" v-model="formData.email" type="text" placeholder="Email">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password">
+              <input class="form-control form-control-lg" v-model="formData.password" type="password" placeholder="Password">
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+            <button @click.prevent="onSubmit" type="submit" class="btn btn-lg btn-primary pull-xs-right">Sign up</button>
           </form>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      formData: {
+        username: "",
+        email: "",
+        password: ""
+    }
+    }
+  },
+  methods: {
+    onSubmit() {
+      const user = {
+        "user": {
+          "username": this.formData.name,
+          "email": this.formData.email,
+          "password": this.formData.password
+        }
+      }
+      console.log(user);
+      axios.post('https://conduit.productionready.io/api/users', user)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    }
+  }
+}
+</script>
