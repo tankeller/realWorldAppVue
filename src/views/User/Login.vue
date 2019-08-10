b06909b0b51ebbdcb80a5d29c24b0caa96c13afc
Tan Keller <ersoichso@gmail.com>
2019-08-10 18:39:37 +0200

    Add login functionality

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -5,27 +5,50 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign up</h1>
           <p class="text-xs-center">
-            <a href>Have an account?</a>
+            <router-link :to="{ name: 'register'}">Need an account?</router-link>
           </p>
 
-          <ul class="error-messages">
+          <!-- <ul class="error-messages">
             <li>That email is already taken</li>
-          </ul>
+          </ul> -->
 
           <form>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Your Name">
+              <input class="form-control form-control-lg" type="text" placeholder="Email" v-model="formData.email">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Email">
+              <input class="form-control form-control-lg" type="password" placeholder="Password" v-model="formData.password">
             </fieldset>
-            <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password">
-            </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+            <button class="btn btn-lg btn-primary pull-xs-right" @click.prevent="onSubmit">
+              Sign up
+            </button>
           </form>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      const user = {
+        user: {
+          email: this.formData.email,
+          password: this.formData.password
+        }
+      }
+      this.$store.dispatch('login', user);
+    }
+  }
+}
+</script>
