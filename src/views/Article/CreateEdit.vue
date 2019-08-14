c32c81b2fa046ff791de51e29959f5fb143ba71a
Tan Keller <ersoichso@gmail.com>
2019-08-14 20:29:20 +0200

    Add form control to post article

--- src/views/Article/CreateEdit.vue
+++ src/views/Article/CreateEdit.vue
@@ -6,23 +6,24 @@
           <form>
             <fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control form-control-lg" placeholder="Article Title">
+                <input v-model="articleData.title" type="text" class="form-control form-control-lg" placeholder="Article Title">
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="What's this article about?">
+                <input v-model="articleData.description" type="text" class="form-control" placeholder="What's this article about?">
               </fieldset>
               <fieldset class="form-group">
                 <textarea
+                  v-model="articleData.body"
                   class="form-control"
                   rows="8"
                   placeholder="Write your article (in markdown)"
                 ></textarea>
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Enter tags">
+                <input @change="addTags" type="text" class="form-control" placeholder="Enter tags">
                 <div class="tag-list"></div>
               </fieldset>
-              <button class="btn btn-lg pull-xs-right btn-primary" type="button">Publish Article</button>
+              <button @click.prevent="onSubmit" class="btn btn-lg pull-xs-right btn-primary" type="button">Publish Article</button>
             </fieldset>
           </form>
         </div>
@@ -30,3 +31,37 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      articleData: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      const article = {
+        article: {
+          title: this.articleData.title,
+          description: this.articleData.description,
+          body: this.articleData.body,
+          tagList: this.articleData.tagList
+        }
+      }
+
+      this.$store.dispatch('createArticle', article);
+    },
+    addTags (event) {
+      this.articleData.tagList = event.target.value.split(',').map(tag => {
+        return tag.trim();
+      });
+    }
+  }
+}
+</script>
