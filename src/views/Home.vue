88579818ec9efd8fbb3bcaf5e623df13bcecd18c
Tan Keller <ersoichso@gmail.com>
2019-08-28 20:23:12 +0200

    Add loggedin check for Your Feed tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,76 @@
 <template>
   <div class="home-page">
-
-  <div class="banner">
-    <div class="container">
-      <h1 class="logo-font">conduit</h1>
-      <p>A place to share your knowledge.</p>
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">conduit</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
     </div>
-  </div>
 
-  <div class="container page">
-    <div class="row">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <div class="feed-toggle">
+            <ul class="nav nav-pills outline-active">
+              <li v-if="auth" class="nav-item">
+                <a class="nav-link disabled" href>Your Feed</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href>Global Feed</a>
+              </li>
+            </ul>
+          </div>
 
-      <div class="col-md-9">
-        <div class="feed-toggle">
-          <ul class="nav nav-pills outline-active">
-            <li class="nav-item">
-              <a class="nav-link disabled" href="">Your Feed</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="">Global Feed</a>
-            </li>
-          </ul>
+          <app-article-list></app-article-list>
         </div>
 
-        <app-article-list></app-article-list>
-        
-      </div>
+        <div class="col-md-3">
+          <div class="sidebar">
+            <p>Popular Tags</p>
 
-      <div class="col-md-3">
-        <div class="sidebar">
-          <p>Popular Tags</p>
-
-          <div class="tag-list">
-            <a :key="index" v-for="(tag, index) in tags" href="" class="tag-pill tag-default">
-              {{ tag }}
-            </a>
+            <div class="tag-list">
+              <a
+                :key="index"
+                v-for="(tag, index) in tags"
+                href
+                class="tag-pill tag-default"
+              >{{ tag }}</a>
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
-
-</div>
 </template>
 
 <script>
-  import axios from 'axios'
-  import ArticleList from '../components/Listing/ArticleList.vue'
+import axios from 'axios';
+import ArticleList from '../components/Listing/ArticleList.vue';
 
-  export default {
-    name: "home",
-    data() {
-      return {
-        tags: {}
-      }
-    },
-    components: {
-      AppArticleList: ArticleList
-    },
-    created () {
-      axios.get('https://conduit.productionready.io/api/tags')
+export default {
+  name: 'home',
+  data () {
+    return {
+      tags: {}
+    };
+  },
+  components: {
+    AppArticleList: ArticleList
+  },
+  created () {
+    axios
+      .get('https://conduit.productionready.io/api/tags')
       .then(res => {
         this.tags = res.data.tags;
-      }).catch((err) => {
-        console.log(err)
+      })
+      .catch(err => {
+        console.log(err);
       });
+  },
+  computed: {
+    auth () {
+      return this.$store.getters.isAuthenticated
     }
-  };
+  }
+};
 </script>
